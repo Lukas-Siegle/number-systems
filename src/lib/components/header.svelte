dd4195e62bf00d9a01072637d754ccf1489884fd
Lukas-Siegle <sieglelukas1@gmail.com>
2024-10-12 11:21:19 +0200

    add repo to header

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -6,7 +6,7 @@
     <div class="container mx-auto flex items-center justify-between">
       <div class="w-8"></div>
       <h1 class="text-3xl font-bold text-center flex-grow">Number Systems</h1>
-      <a href="https://github.com/" target="_blank" rel="noopener noreferrer" class="text-white hover:text-gray-200 transition-colors">
+      <a href="https://github.com/Lukas-Siegle/number-systems" target="_blank" rel="noopener noreferrer" class="text-white hover:text-gray-200 transition-colors">
         <Github size={32} />
       </a>
     </div>
