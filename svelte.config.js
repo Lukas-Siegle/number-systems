62590f265674d879dfb8d7271859147a4e615027
Lukas-Siegle <sieglelukas1@gmail.com>
2024-11-03 11:36:30 +0100

    deploy to gh pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,27 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     preprocess: vitePreprocess(),
+
     kit: {
         adapter: adapter({
-        }
-        )
+            pages: 'build',
+            assets: 'build',
+            fallback: '404.html',  // Add this for proper routing and 404 handling
+            strict: true
+        }),
+        alias: {
+            "@/*": "./path/to/lib/*",
+        },
+        prerender: {
+            entries: ['*', '/404'],  // Add /404 to prerender entries
+            handleHttpError: 'warn'   // Optional: helps with prerender errors
+        },
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
     }
 };
 
